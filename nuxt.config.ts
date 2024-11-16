ed994f2c196b5d57aae6e85821843488ef1b9c18
simpaul13 <28johnpaulsim28@gmail.com>
2024-11-16 23:46:16 +0800

    Config color-mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,16 @@ export default defineNuxtConfig({
   ],
 
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+
+  colorMode: {
+    preference: 'system', // default value
+    fallback: 'light', // default value
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '-mode',
+    storage: 'localStorage',
+    storageKey: 'nuxt-color-mode'
+  }
 })
