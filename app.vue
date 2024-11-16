2fe101ac50e1f2aa1e0252802aac18be6a2f8436
simpaul13 <28johnpaulsim28@gmail.com>
2024-11-17 02:02:49 +0800

    Add Backgrond dark and light mode

--- app.vue
+++ app.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>
+  <div class="dark:bg-slate-800 dark:text-gray-300">
     <Header />
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <Skills />
     <Footer />
   </div>
 </template>
